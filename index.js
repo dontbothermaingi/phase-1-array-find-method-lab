b3b7723f3f4716c5888b02cf39cc79fb04c1501c
+ <+>
2023-12-18 13:50:33 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    // Find the first entry in the record where the result is a win ("W")
+    const winningEntry = record.find(element => element.result === "W");
+  
+    // Return the year if a win is found, otherwise return undefined
+    return winningEntry ? winningEntry.year : undefined;
+  }
+  
+console.log(superbowlWin())
